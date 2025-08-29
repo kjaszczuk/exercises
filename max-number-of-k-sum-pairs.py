5b8c209c7c713a21019dfe9e41b5086da47f687f
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-08-29 20:52:20 +0200

    Update max-number-of-k-sum-pairs.py

--- max-number-of-k-sum-pairs.py
+++ max-number-of-k-sum-pairs.py
@@ -1,26 +1,25 @@
-# https://leetcode.com/problems/max-number-of-k-sum-pairs/?envType=study-plan-v2&envId=leetcode-75
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         counter = 0
+        nums.sort()
+        limit = len(nums)
+
+        # get rid of values that are too high
+        for i in range (len(nums)-1,-1,-1):
+            if nums[i] < k:
+                limit = i+1
+                break
+        r = limit
+        nums = nums[:limit]
         i = 0
-        flag_popped = False
-        while i < len(nums):
-            if nums[i] >= k:
-                nums.pop(i)
-                continue
-            i += 1
-        i = 0
-        while i < len(nums):
-            for j in range (i+1, len(nums)):
-                # print(i, nums, counter)
-                if nums[i]+nums[j] == k:
-                    nums.pop(i)
-                    nums.pop(j-1)
-                    counter += 1
-                    flag_popped = True
-                    # print(i, nums, counter)
-                    break
-            if not flag_popped:
-                i += 1
-            flag_popped = False
+
+        # try to solve it using count()
+        while i < len(nums) and nums[i] < k/2:
+            # print(i, nums[i], nums)
+            i_count = nums.count(nums[i])
+            counter += min(i_count, nums.count(k-nums[i]))
+            i += i_count
+
+        counter += floor(nums.count(k/2)/2)
+
         return counter
