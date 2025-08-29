b60340e7fe4985b3ce7336577c7033e963e23f92
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-08-29 21:17:54 +0200

    previous solution had 46/51 tests passed (time exceeded), this one has 46/51

--- max-number-of-k-sum-pairs.py
+++ max-number-of-k-sum-pairs.py
@@ -1,25 +1,23 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         counter = 0
-        nums.sort()
-        limit = len(nums)
-
-        # get rid of values that are too high
-        for i in range (len(nums)-1,-1,-1):
-            if nums[i] < k:
-                limit = i+1
-                break
-        r = limit
-        nums = nums[:limit]
+        
         i = 0
 
+        checked_values = []
+
         # try to solve it using count()
-        while i < len(nums) and nums[i] < k/2:
-            # print(i, nums[i], nums)
-            i_count = nums.count(nums[i])
-            counter += min(i_count, nums.count(k-nums[i]))
-            i += i_count
-
-        counter += floor(nums.count(k/2)/2)
-
+        while i < len(nums):
+            if nums[i] in checked_values:
+                i += 1
+                continue
+            elif nums[i] == k/2:
+                counter += floor(nums.count(k/2)/2)
+                checked_values.append(nums[i])
+                i += 1
+                continue
+            counter += min(nums.count(nums[i]), nums.count(k-nums[i]))
+            checked_values.append(nums[i])
+            checked_values.append(k-nums[i])
+            i += 1
         return counter
