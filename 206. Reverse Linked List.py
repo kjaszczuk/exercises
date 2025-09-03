5c3e5bf7c3e3ae4b7f945c0f8d1aabd798022326
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-09-04 00:01:12 +0200

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -5,12 +5,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current = head
-        previous = None
-        while current:
-            next_node = current.next # make copy of next
-            current.next = previous # reassign it to previous (for head it's None)
-            previous = current # save current to previous
-            current = next_node # move to next
-        return previous
-      # 100, 79
+        # iteratively
+        
+        # current = head
+        # previous = None
+        # while current:
+        #     next_node = current.next # make copy of next
+        #     current.next = previous # reassign it to previous (for head it's None)
+        #     previous = current # save current to previous
+        #     current = next_node # move to next
+        # return previous
+        # 100 79
+
+        # recursively
+        if not head or not head.next:
+            return head
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+        # 100 5
