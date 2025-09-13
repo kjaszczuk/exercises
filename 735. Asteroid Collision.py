a74a4a0bae30f06c50ea30ea3d755b49e878db58
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-09-13 17:53:43 +0200

    Update and rename 2390. Removing Stars From a String.py to 735. Asteroid Collision.py

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -1,24 +1,26 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-# misunderstood directions - not passing tests
         stack = []
         flag_append = False
         for i in asteroids:
             # add to stack if it's empty, or asteroids move in the same direction
-            if not stack or stack[-1]*i > 0:
+            if not stack or stack[-1]*i > 0 or (stack[-1] < 0 and i > 0):
                 stack.append(i)
             else:
-                while stack or stack[-1]*i < 0:
-                    if abs(stack[-1]) < abs(i):
+                while stack and (stack[-1]*i < 0 or (stack[-1] > 0 and i < 0)):
+                    if stack[-1] < i*-1: # faster than abs (with abs it was 59,19)
                         stack.pop()
                         flag_append = True
+                        continue
                     elif stack[-1] == i*-1:
                         stack.pop()
                         flag_append = False
-                        break
+                    elif stack[-1] == i:
+                        flag_append = True
                     else:
                         flag_append = False
-                        break
+                    break
                 if flag_append:
                     stack.append(i)
         return stack
+# 94, 19
