9c44779378302ab4819462c0453fac0c030fa8aa
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-09-14 11:01:22 +0200

    Update 649. Dota2 Senate.py

--- 649. Dota2 Senate.py
+++ 649. Dota2 Senate.py
@@ -1,33 +1,19 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        queue = list(senate)
-        last_senator = ''
-        votes = 0
-        i = 0
-        while True:
-            while i < len(queue):
-                if votes == 0 or queue[i] == last_senator:
-                    last_senator = queue[i]
-                    votes += 1
-                    i += 1
-                elif votes > 0:
-                    del queue[i]
-                    votes -= 1
-            i = 0
-            for j in range (votes):
-                if last_senator == 'R' and "D" in queue:
-                    del queue[queue.index('D')]
-                elif last_senator == 'D' and "R" in queue:
-                    del queue[queue.index('R')]
-            votes = 0     
+        n = len(senate)
 
-            if "R" not in queue:
-                return 'Dire'
-            if "D" not in queue:
-                return 'Radiant' 
-# who wins:
-    # 2vs3 L
-    # 3 vs 5 P
-    # 4 vs 7 P
-
-# 8, 52
+        rad = [i for i in range (len(senate)) if senate[i] == 'R']
+        dir = [i for i in range (len(senate)) if senate[i] == 'D']
+        
+        while rad and dir:
+            r = rad.pop(0)
+            d = dir.pop(0)
+            if r < d:
+                rad.append(n+r)
+            else:
+                dir.append(n+d)
+        if rad:
+            return 'Radiant'
+        else:
+            return 'Dire'
+# 16, 19
