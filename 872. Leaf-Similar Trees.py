c5c28fdf3b96bff1889667d467566f135c532cfa
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-09-21 21:20:30 +0200

    Update 872. Leaf-Similar Trees.py

--- 872. Leaf-Similar Trees.py
+++ 872. Leaf-Similar Trees.py
@@ -11,31 +11,41 @@ class Solution:
 # tylko gdy wezel nie ma lisci to dopisujemy go do listy, ktora bedziemy porownywac
     # optymalnie byloby przechodzic przez oba drzewa jednoczesnie, zeby jak bedzie roznica od razu zwrocic false (porownujemy listy obciete do w tym momencie krotszej)
 #najpierw zrob dzialajacy przechodzacy oba w calosci jeden po drugim
-# rekurencyjnie raczej sie nie da (bo 2 drzewa)
 
-        stack1 = [root1]
-        stack2 = [root2]
-        leaves1 = []
-        leaves2 = []
+# #iterative
+#         stack1 = [root1]
+#         stack2 = [root2]
+#         leaves1 = []
+#         leaves2 = []
 
-        while stack1:
-            node1 = stack1.pop()
-            if node1.right or node1.left:
-                if node1.right:
-                    stack1.append(node1.right)
-                if node1.left:
-                    stack1.append(node1.left)
-            else:
-                leaves1.append(node1.val)
+#         while stack1:
+#             node1 = stack1.pop()
+#             if node1.right or node1.left:
+#                 if node1.right:
+#                     stack1.append(node1.right)
+#                 if node1.left:
+#                     stack1.append(node1.left)
+#             else:
+#                 leaves1.append(node1.val)
 
-        while stack2:
-            node2 = stack2.pop()
-            if node2.right or node2.left:
-                if node2.right:
-                    stack2.append(node2.right)
-                if node2.left:
-                    stack2.append(node2.left)
-            else:
-                leaves2.append(node2.val)
-        return leaves1 == leaves2
-# 100 44
+#         while stack2:
+#             node2 = stack2.pop()
+#             if node2.right or node2.left:
+#                 if node2.right:
+#                     stack2.append(node2.right)
+#                 if node2.left:
+#                     stack2.append(node2.left)
+#             else:
+#                 leaves2.append(node2.val)
+#         return leaves1 == leaves2
+# # 100 44
+
+#recursive
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+        return list(dfs(root1)) == list(dfs(root2))
+# 8-100, 94
