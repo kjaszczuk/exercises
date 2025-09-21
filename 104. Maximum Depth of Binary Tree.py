8e7b32b04e09b563ba5fc8f76c434047f0a13d7c
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-09-21 11:13:14 +0200

    Update 104. Maximum Depth of Binary Tree.py

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -10,4 +10,25 @@ class Solution:
         if not root:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
-        # 100 78
+# 100 78
+        
+#iterative
+        if not root:
+            return 0
+        max_d = 1
+        depth = 1
+        stack = [(root, depth)]
+
+        while stack:
+            # print(stack)
+            cur = stack.pop()
+            depth = cur[1]
+            max_d = max(max_d, depth)
+            if cur[0].left or cur[0].right:
+                depth += 1
+                if cur[0].left:
+                    stack.append((cur[0].left, depth))
+                if cur[0].right:
+                    stack.append((cur[0].right, depth))
+        return max_d
+# 37, 29
