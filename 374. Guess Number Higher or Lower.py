a92ba71d3f9fc34ef5b430d381e3313dd693e764
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-10-19 18:31:30 +0200

    Add guessNumber method to Solution class
    Implement binary search for guessing the number.

---
+++ 374. Guess Number Higher or Lower.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        n = n + 1
+        while True:
+            num = (low + n)//2
+            guess_result = guess(num)
+            if guess_result == 0:
+                return num
+            if guess_result == -1:
+                n = (low + n)//2
+            if guess_result == 1:
+                low = (low + n)//2
+# 27, 29
