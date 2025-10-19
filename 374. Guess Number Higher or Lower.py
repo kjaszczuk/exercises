82ea9622191bb6b0e215b82d4c7bc2c8dbb40150
kj <34030768+kjaszczuk@users.noreply.github.com>
2025-10-19 19:05:15 +0200

    Update 374. Guess Number Higher or Lower.py

--- 374. Guess Number Higher or Lower.py
+++ 374. Guess Number Higher or Lower.py
@@ -7,15 +7,26 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        low = 1
-        n = n + 1
-        while True:
-            num = (low + n)//2
-            guess_result = guess(num)
-            if guess_result == 0:
-                return num
-            if guess_result == -1:
-                n = (low + n)//2
-            if guess_result == 1:
-                low = (low + n)//2
+        # low = 1
+        # n = n + 1
+        # while True:
+        #     num = (low + n)//2
+        #     guess_result = guess(num)
+        #     if guess_result == 0:
+        #         return num
+        #     if guess_result == -1:
+        #         n = (low + n)//2
+        #     if guess_result == 1:
+        #         low = (low + n)//2
 # 27, 29
+
+        low, high = 1, n
+        myguess = (1+n) >> 1
+        while ((res := guess(myguess)) != 0):
+            if res == -1:
+                high = myguess - 1
+            else:
+                low = myguess + 1
+            myguess = (low+high) >> 1
+        return myguess
+# 57, 29
